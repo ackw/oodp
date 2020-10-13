61a530940315f08a2c35ec541b17c131a69aedfa
a <andrelchew@icloud.com>
2020-10-13 22:46:35 +0800

    file adjustment

---
+++ src/version1/Course.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Course {
+    public static void courseList(){
+        try {
+            Scanner scan = new Scanner(new File("src/data/course.txt"));
+            Main.skipLine(scan, 1);
+            while (scan.hasNextLine()) {
+                String s = scan.nextLine();
+                String[] sArray = s.split(",");
+
+                for (int i = 0; i < sArray.length; i++) {
+                    System.out.println(sArray[i]);
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
