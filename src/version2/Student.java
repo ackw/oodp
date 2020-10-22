524b51494db2801dfc0e2a6bb5af857db8fd10b0
LH <p.lianghong@gmail.com>
2020-10-23 01:06:06 +0800

    revertz

--- src/version2/Student.java
+++ src/version2/Student.java
@@ -6,13 +6,13 @@ public class Student extends User
     private char gender;
     private String nationality;
     private ArrayList registeredCourseList = new ArrayList();
-    private Calendar startDate, endDate;
+    private Calendar startTime, endTime;
 
     public Student()
     {
         super();
-        startDate = Calendar.getInstance();
-        endDate = Calendar.getInstance();
+        startTime = Calendar.getInstance();
+        endTime = Calendar.getInstance();
     }
     
     public Student(String n, String u, String p, Boolean t, int m, char g, String nat)
@@ -21,8 +21,8 @@ public class Student extends User
         matricNumber = m;
         gender = g;
         nationality = nat;
-        startDate = Calendar.getInstance();
-        endDate = Calendar.getInstance();
+        startTime = Calendar.getInstance();
+        endTime = Calendar.getInstance();
     }
 
     public int getMatricNumber(){return matricNumber;}
@@ -34,19 +34,16 @@ public class Student extends User
     public String getNationality(){return nationality;}
     public void setNationality(String nat){nationality = nat;}
 
-    public Calendar getStartDate(){return startDate;} 
-    public void setStartDate(Calendar d){startDate = d;}
+    public Calendar getStartTime(){return startTime;} 
+    public void setStartTime(Calendar d){startTime = d;}
 
-    public Calendar setEndDate(){return endDate;} 
-    public void setEndDate(Calendar d){endDate = d;}
+    public Calendar setEndTime(){return endTime;} 
+    public void setEndTime(Calendar d){endTime = d;}
+
+    
 
     public String toString()
     {
        return super.toString() + "\nMatric Number: " + matricNumber + "\nGender: " + gender + "\nNationality: " + nationality;
-    }
-
-    public String dateToString()
-    {
-        return "startDate: " + startDate.get(Calendar.DATE) + "/" + startDate.get(Calendar.MONTH) + "/" + startDate.get(Calendar.YEAR);
     }
 }
