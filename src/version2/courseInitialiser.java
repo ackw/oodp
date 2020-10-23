621e7051a8f583dd227f185ded89b5db4f0fa8e0
hellomarshmellow <70008770+hellomarshmellow@users.noreply.github.com>
2020-10-23 14:11:13 +0800

    fixed courseinitialiser file

--- src/version2/courseInitialiser.java
+++ src/version2/courseInitialiser.java
@@ -11,32 +11,32 @@ public class courseInitialiser {
         ArrayList<Object> returnlist = new ArrayList<Object>();
         ArrayList<Object> courseList = new ArrayList<Object>();
 
-        // // //Writing arraylist to the file
-        // Course c;
-        // c = new Index("CZ2002", "SCSE", 20021, 20);
-        // courseList.add(c);
-        // c = new Index("CZ2002", "SCSE", 20022, 25);
-        // courseList.add(c);
-        // c = new Index("CZ2003", "SCSE", 20031, 20);
-        // courseList.add(c);
-        // c = new Index("CZ2003", "SCSE", 20032, 30);
-        // courseList.add(c);
-        // c = new Index("HG2024", "SOH", 20241, 15);
-        // courseList.add(c);
-        // c = new Index("HG2024", "SOH", 20242, 20);
-        // courseList.add(c);
+        //Writing arraylist to the file
+        Course c;
+        c = new Index("CZ2002", "SCSE", 20021, 20);
+        courseList.add(c);
+        c = new Index("CZ2002", "SCSE", 20022, 25);
+        courseList.add(c);
+        c = new Index("CZ2003", "SCSE", 20031, 20);
+        courseList.add(c);
+        c = new Index("CZ2003", "SCSE", 20032, 30);
+        courseList.add(c);
+        c = new Index("HG2024", "SOH", 20241, 15);
+        courseList.add(c);
+        c = new Index("HG2024", "SOH", 20242, 20);
+        courseList.add(c);
 
-        // try {
-        //     FileOutputStream fos = new FileOutputStream("./src/data/courseinfo");
-        //     ObjectOutputStream oos = new ObjectOutputStream(fos);   
-        //     oos.writeObject(courseList);
-        //     oos.flush();
-        //     oos.close();
-        //     }
+        try {
+            FileOutputStream fos = new FileOutputStream("./src/data/courseinfo");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);   
+            oos.writeObject(courseList);
+            oos.flush();
+            oos.close();
+            }
     
-        // catch(Exception ex) {
-        //     ex.printStackTrace();
-        // }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
 
         //Reading arraylist from the file
         try {
